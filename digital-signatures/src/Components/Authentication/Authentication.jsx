28c90605fbfa2f3087a4c9d4234bebb34d74f82f
Joseph <jLegand@retrorabbit.co.za>
2019-04-12 13:21:55 +0200

    Added Roboto font to the solution
    Created the styling for the Authentication component

--- digital-signatures/src/Components/Authentication/Authentication.jsx
+++ digital-signatures/src/Components/Authentication/Authentication.jsx
@@ -5,7 +5,8 @@ import * as AuthenticationAction from './AuthenticationAction';
 import { bindActionCreators } from 'redux';
 import queryString from 'query-string';
 import base64 from 'base-64';
-import { ToastContainer,toast } from 'mdbreact';
+import { ToastContainer, toast } from 'mdbreact';
+import './Authentication.scss';
 
 class Authentication extends React.Component {
   constructor(props) {
@@ -19,7 +20,7 @@ class Authentication extends React.Component {
 
 
   handleChange(event) {
-    if(event.which===13)event.preventDefault();
+    if (event.which === 13) event.preventDefault();
     this.setState({ ID: event.target.value })
   }
 
@@ -31,11 +32,11 @@ class Authentication extends React.Component {
   }
 
   DecodeLink(link) {
-    var decodedLink,decodedURI;
-    try{
-       decodedLink = base64.decode(link);
-       decodedURI = decodeURIComponent(decodedLink);
-    }catch(err){
+    var decodedLink, decodedURI;
+    try {
+      decodedLink = base64.decode(link);
+      decodedURI = decodeURIComponent(decodedLink);
+    } catch (err) {
       toast.error("Inavlid LINK, Redirect to link sent via sms/email")
     }
     return decodedURI;
@@ -50,32 +51,61 @@ class Authentication extends React.Component {
     return (
       <Fragment>
         <ToastContainer
-                        hideProgressBar={true}
-                        newestOnTop={true}
-                        autoClose={5000}
-                    />
-        <div>
-      <div className="container">
-        <form onKeyPress={this.handleChange}>
-          <div className="form-group row">
-            <label className="col-sm-2 col-form-label">ID Number</label>
-            <div className="col-sm-10">
-              <input type="text"
-                className="form-control"
-                id="IdNumber"
-                placeholder="ID Number"
-                value={this.state.ID}
-                onChange={this.handleChange} />
-            </div>
+          hideProgressBar={true}
+          newestOnTop={true}
+          autoClose={5000}
+        />
+        <div className="Authentication">
+          <div className="container">
+            <form onKeyPress={this.handleChange}>
+              <div className="form-group rectangle mx-auto">
+                <div className="row">
+                  <div className="col">
+                    <div className="row">
+                      <div className="col">
+                        <div className="row">
+                          <div className="col-sm-4 mx-auto m-3">
+                            <span className="please-enter-your-id-copy">Please enter your ID number</span>
+                          </div>
+                        </div>
+
+                      </div>
+                    </div>
+                    <div className="row">
+                      <div className="col">
+                        <div className="row">
+                          <div className="col-sm-4 mx-auto">
+                            <input type="text"
+                              className="form-control rectangle-copy"
+                              id="IdNumber"
+                              placeholder="ID Number"
+                              value={this.state.ID}
+                              onChange={this.handleChange} />
+                          </div>
+                        </div>
+
+                      </div>
+                    </div>
+                    <div className="row">
+                      <div className="col">
+                        <div className="form-group">
+                          <div className="row">
+                            <div className=" col-sm-4 mx-auto m-3">
+                              <button type="button"
+                                className="btn rectangle-copy-2"
+                                onClick={() => this.Authenticate()}><span className="submit-copy">SUBMIT</span></button>
+                            </div>
+                          </div>
+
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </form>
           </div>
-          <div className="form-group row">
-            <div className="offset-sm-2 col-sm-10">
-              <button type="button" className="btn btn-primary" onClick={() => this.Authenticate()}>Authenticate</button>
-            </div>
-          </div>
-        </form>
-      </div>
-    </div>
+        </div>
       </Fragment>
     );
   }
