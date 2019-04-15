2dfb6f8de36b3cf9668efe59bdde7b948ef6d616
Joseph <jLegand@retrorabbit.co.za>
2019-04-15 10:56:50 +0200

    Added styling for the Confirmation component and responsiveness

--- digital-signatures/src/Components/Confirmation/Confirmation.jsx
+++ digital-signatures/src/Components/Confirmation/Confirmation.jsx
@@ -1,11 +1,63 @@
-import React from 'react';
-
+import React from "react";
+import "./Confirmation.scss";
+import Success from "../../Images/Silica_success_icon-01.svg";
+import Success2 from "../../Images/Silica_success_icon-02.svg";
 class Confirmation extends React.Component {
-  state = {}
+  state = {};
   render() {
-    return (<div className="container">
-      Thank you very much for your feedback!
-        </div>);
+    return (
+      <div className="Confirmation">
+        <div className="container">
+          <div className="row">
+            <div className="col">
+              <div className="rectangle text-center">
+                <div className="row">
+                  <div className="col">
+                    <div className="row pt-5 m-3">
+                      <div className="col">
+                        <img
+                          className="oval-copy-2"
+                          alt="sucess-logo"
+                          src={Success}
+                        />
+                      </div>
+                    </div>
+                    <div className="row m-3">
+                      <div className="col">
+                        <img
+                          className="sml-screen"
+                          alt="sucess-logo"
+                          src={Success2}
+                        />
+                      </div>
+                    </div>
+                    <div className="row">
+                      <div className="col">
+                        <h2 className="success-copy">Success!</h2>
+                      </div>
+                    </div>
+                    <div className="row">
+                      <div className="col">
+                        <p className="your-response-has-be-copy">
+                          Your response has been recieved, and will be processed
+                        </p>
+                      </div>
+                    </div>
+                    <div className="row">
+                      <div className="col">
+                        <button className="btn btn-primary">Continue</button>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div />
+        </div>
+      </div>
+    );
   }
 }
 
